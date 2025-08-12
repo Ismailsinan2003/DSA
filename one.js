53984710148138c9f343a2abfbca337a23e75c1c
Ismail Sinan <143427342+Ismailsinan2003@users.noreply.github.com>
2025-08-12 00:19:37 +0000

    patterns

--- one.js
+++ one.js
@@ -16,9 +16,32 @@ function same(arr1, arr2) {
             return false
         }
     }
+    console.log(squared)
+    console.log(input)
     return true
 }
-console.log(same([1,2,1], [4,4,1]))
+console.log(same([1,2,3,4], [9,4,1,15]))
+
+
+
+///////     Naive Approach
+// function same(arr1, arr2){
+//     if (arr1.length!=arr2.length) {
+//         return false
+//     }
+
+//     for (let i = 0; i < arr1.length; i++) {
+//         let correctIndex=arr2.indexOf(arr1[i] **2)
+//         if(correctIndex === -1){
+//             return false
+//         }
+//         console.log(arr2)
+//         arr2.splice(correctIndex, 1)
+//     }
+//     return true
+// }
+
+// console.log(same([1,2,3], [9,4,1]))
 
 
 
